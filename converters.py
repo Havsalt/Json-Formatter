a48d54f84a0bd815d82609a351cdb28ab232215c
FloatingInt <knutolaihelgesen@gmail.com>
2024-02-16 17:49:05 +0100

    Using underscore to mark private implementation

--- converters.py
+++ converters.py
@@ -1,47 +1,47 @@
-from annotations import Converter
+from annotations import Converter as _Converter
 
 
-EMPTY = ""
-KEBAB = "-"
-TAIL = "_"
-NORM_SUFFIX = "_norm"
+_EMPTY = ""
+_KEBAB = "-"
+_TAIL = "_"
+_NORM_SUFFIX = "_norm"
 
 
-def get_default_converter() -> Converter:
+def get_default_converter() -> _Converter:
     return norm
 
 
-def get_norm_converter(function: Converter) -> Converter:
+def get_norm_converter(function: _Converter) -> _Converter:
     # NOTE: globals from this' module namespace
-    return globals().get(function.__name__ + NORM_SUFFIX, get_default_converter())
+    return globals().get(function.__name__ + _NORM_SUFFIX, get_default_converter())
 
 
 def norm(string: str) -> str:
-    return (string.replace(2*KEBAB, KEBAB)
-                  .replace(2*KEBAB, KEBAB)
-                  .replace(2*TAIL, TAIL)
-                  .replace(2*TAIL, TAIL)
-                  .strip(KEBAB + TAIL))
+    return (string.replace(2*_KEBAB, _KEBAB)
+                  .replace(2*_KEBAB, _KEBAB)
+                  .replace(2*_TAIL, _TAIL)
+                  .replace(2*_TAIL, _TAIL)
+                  .strip(_KEBAB + _TAIL))
 
 
 def snake(string: str) -> str:
-    if string == EMPTY:
+    if string == _EMPTY:
         return string
     is_pascal_or_upper = string[0].isupper()
     final = ""
-    for idx, char in enumerate(string.replace(KEBAB, TAIL)):
+    for idx, char in enumerate(string.replace(_KEBAB, _TAIL)):
         if idx != 0 and idx != len(string) -1:
             if not is_pascal_or_upper and char.isupper():
-                final += TAIL
+                final += _TAIL
         final += char.lower()
     return final
 
 
 def snake_norm(string: str) -> str:
-    return (snake(string).replace(KEBAB, TAIL)
-                         .replace(2*TAIL, TAIL)
-                         .replace(2*TAIL, TAIL)
-                         .strip(KEBAB + TAIL))
+    return (snake(string).replace(_KEBAB, _TAIL)
+                         .replace(2*_TAIL, _TAIL)
+                         .replace(2*_TAIL, _TAIL)
+                         .strip(_KEBAB + _TAIL))
 
 
 def camal(string: str) -> str:
@@ -49,7 +49,7 @@ def camal(string: str) -> str:
     was_binder = False
     for idx, char in enumerate(string):
         if idx != 0:
-            if char == TAIL or char == KEBAB:
+            if char == _TAIL or char == _KEBAB:
                 was_binder = True
                 continue
         if was_binder:
@@ -63,35 +63,34 @@ def camal(string: str) -> str:
     
 
 def camal_norm(string: str) -> str:
-    return camal(string).strip(KEBAB + TAIL)
+    return camal(string).strip(_KEBAB + _TAIL)
 
 
 def kebab(string: str) -> str:
-    if string == EMPTY:
+    if string == _EMPTY:
         return string
     is_pascal_or_upper = string[0].isupper()
     final = ""
-    for idx, char in enumerate(string.replace(TAIL, KEBAB)):
+    for idx, char in enumerate(string.replace(_TAIL, _KEBAB)):
         if idx != 0 and idx != len(string) -1:
             if not is_pascal_or_upper and char.isupper():
-                final += KEBAB
+                final += _KEBAB
         final += char.lower()
     return final
 
 
 def kebab_norm(string: str) -> str:
-    return (kebab(string).replace(2*KEBAB, KEBAB)
-                         .replace(2*KEBAB, KEBAB)
-                         .strip(KEBAB + TAIL))
+    return (kebab(string).replace(2*_KEBAB, _KEBAB)
+                         .replace(2*_KEBAB, _KEBAB)
+                         .strip(_KEBAB + _TAIL))
 
 
 def pascal(string: str) -> str:
     raw = camal(string)
-    if raw == EMPTY:
+    if raw == _EMPTY:
         return raw
     return raw[0].upper() + raw[1:]
 
 
 def pascal_norm(string: str) -> str:
-    return pascal(string).strip(KEBAB + TAIL)
-
+    return pascal(string).strip(_KEBAB + _TAIL)
