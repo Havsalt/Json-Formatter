e9e6e1539da15c8690591d0eb197964647ca7f2f
FloatingInt <knutolaihelgesen@gmail.com>
2024-02-16 17:50:32 +0100

    ParserArgs now extends argparse.Namespace

--- annotations.py
+++ annotations.py
@@ -1,10 +1,11 @@
-from typing import TypeAlias, Callable
+from argparse import Namespace as _Namespace
+from typing import TypeAlias as _TypeAlias, Callable as _Callable
 
 
-Converter: TypeAlias = Callable[[str], str]
+Converter: _TypeAlias = _Callable[[str], str]
 
 
-class ParserArgs:
+class ParserArgs(_Namespace):
     path: str
     indent: int
     format: str | None
